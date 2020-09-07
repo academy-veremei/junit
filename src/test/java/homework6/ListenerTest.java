dc88342ec680e8cca0277b1899c8d81b87ec54ea
academy-veremei <veremei.d@gmail.com>
2020-09-07 04:36:20 +0300

    Homework #6 Stream.Lambda.JUnit.

--- src/test/java/homework6/ListenerTest.java
+++ src/test/java/homework6/ListenerTest.java
@@ -1,7 +1,6 @@
 package homework6;
 
 import org.junit.runner.JUnitCore;
-import org.junit.runners.model.InitializationError;
 
 public class ListenerTest {
     public static void main(String[] args) {
