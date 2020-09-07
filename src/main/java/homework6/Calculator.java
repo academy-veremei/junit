67905aebdafadddba84fe4b2b210f58ed814ddc5
academy-veremei <veremei.d@gmail.com>
2020-09-07 04:23:07 +0300

    Homework #6 Stream.Lambda.JUnit.

---
+++ src/main/java/homework6/Calculator.java
@@ -0,0 +1,17 @@
+package homework6;
+
+public class Calculator {
+
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+
+}
